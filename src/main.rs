cca4a43c9e2e71ec5de84343ece6f6e044d25fdc
Chris West (Faux) <git@goeswhere.com>
2017-03-14 14:19:37 +0000

    parse some of a document and write it into postgres

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 #![feature(io)]
 
+extern crate postgres;
+
 use std::env;
 use std::fs;
 use std::io;
+use std::path;
 
 use std::iter::Peekable;
 use std::vec::Vec;
@@ -42,11 +45,71 @@ fn drop_whitespace<T: io::Read>(iter: &mut Stream<T>) {
     }
 }
 
-fn extract_document<T: io::Read>(mut iter: &mut Stream<T>, buf: &Vec<char>)
-    -> Result<(), String>
+fn read_doc<T: io::Read>(mut iter: &mut Stream<T>, mut buf: &mut Vec<char>) -> Result<(), String> {
+    assert_eq!('{', iter.next().unwrap());
+    buf.push('{');
+    loop {
+        drop_whitespace(&mut iter);
+        read_string(&mut iter, &mut buf)?;
+        drop_whitespace(&mut iter);
+        assert_eq!(':', iter.next().unwrap());
+        buf.push(':');
+        drop_whitespace(&mut iter);
+        extract_document(&mut iter, &mut buf);
+        drop_whitespace(&mut iter);
+        let end = iter.next().unwrap();
+        buf.push(end);
+        match end {
+            ',' => continue,
+            '}' => break,
+            _ => return Err(format!("found {} while trying to read document", end)),
+        };
+    }
+    return Ok(());
+}
+
+fn read_array<T: io::Read>(mut iter: &mut Stream<T>, buf: &Vec<char>) -> Result<(), String> {
+    unimplemented!();
+}
+
+fn read_string<T: io::Read>(mut iter: &mut Stream<T>, mut buf: &mut Vec<char>) -> Result<(), String> {
+    assert_eq!('"', iter.next().unwrap());
+    buf.push('"');
+    loop {
+        let c = iter.next().unwrap();
+        buf.push(c);
+        if c == '\\' {
+            if iter.peek() == '"' {
+                panic!("can't do strings with escaped quotes: lazy");
+            }
+        }
+        if c == '"' {
+            break;
+        }
+    }
+    return Ok(());
+}
+
+fn read_num<T: io::Read>(mut iter: &mut Stream<T>, buf: &Vec<char>) -> Result<(), String> {
+    unimplemented!();
+}
+
+fn extract_document<T: io::Read>(mut iter: &mut Stream<T>, mut buf: &mut Vec<char>) -> Result<(), String>
 {
     drop_whitespace(&mut iter);
-    return Err("oops".to_string());
+
+    let start = iter.peek();
+    if start.is_digit(10) || '-' == start {
+        read_num(&mut iter, &mut buf)?;
+    } else {
+        match start {
+            '{' => read_doc(&mut iter, &mut buf),
+            '[' => read_array(&mut iter, &mut buf),
+            '"' => read_string(&mut iter, &mut buf),
+            _ => Err(format!("invalid token start: {}", start)),
+        }?
+    }
+    return Ok(());
 }
 
 fn main() {
@@ -54,8 +117,34 @@ fn main() {
     let us = args.next().expect("binary name must always be present??");
     let path = args.next().expect("input filename must be provided");
     let mut file = fs::File::open(path).expect("input file must exist and be readable");
-    let mut buf: Vec<char> = Vec::new();
     let mut iter = Stream::new(&mut file);
+
+    let params = postgres::params::ConnectParams::builder()
+        .user("faux", None)
+        .build(postgres::params::Host::Unix(
+                path::PathBuf::from("/var/run/postgresql")));
+    let conn = postgres::Connection::connect(params, postgres::TlsMode::None).unwrap();
+    let tran = conn.transaction().unwrap();
+    let stmt = tran.prepare("INSERT INTO db3j (row) VALUES ($1::varchar::jsonb)").unwrap();
+    println!("{}", stmt.param_types()[0]);
+
+    let mut buf: Vec<char> = Vec::new();
     assert_eq!('[', iter.next().expect("non-empty file"));
-    extract_document(&mut iter, &mut buf).expect("suc");
+    loop {
+        extract_document(&mut iter, &mut buf).expect("suc");
+        drop_whitespace(&mut iter);
+        let end = iter.next().unwrap();
+        if end == ',' {
+            let s: String = buf.iter().cloned().collect();
+            stmt.execute(&[&s]).unwrap();
+            buf.clear();
+            continue;
+        }
+        if ']' == end {
+            break;
+        }
+
+        panic!("invalid ender: {}", end);
+    }
+    tran.commit().unwrap();
 }
